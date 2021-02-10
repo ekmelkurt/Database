237d6434bfa01b9c4c96af0904053f16d1313670
DELL <DELL@DESKTOP-L230P3N>
2021-02-10 19:26:32 +0300

    Add Project

---
+++ Database1/Brands.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Brands]
+(
+	[BrandId] INT NOT NULL PRIMARY KEY, 
+    [BrandName] NCHAR(10) NULL
+)
---
+++ Database1/Cars.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Cars]
+(
+	[Id] INT NOT NULL , 
+    [BrandId] INT NOT NULL, 
+    [ColorId] INT NOT NULL, 
+    [ModelYear] INT NULL, 
+    [DailyPrice] INT NULL, 
+    [Description] VARCHAR(50) NULL,
+    CONSTRAINT [FK_Cars_Colors] FOREIGN KEY ([ColorId]) REFERENCES [Colors]([ColorId]), 
+    CONSTRAINT [FK_Cars_Brands] FOREIGN KEY ([BrandId]) REFERENCES [Brands]([BrandId]),
+
+)
